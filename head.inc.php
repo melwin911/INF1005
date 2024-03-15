ded8bea13bb68d920a8b3433084849d9b688cdfb
himhim24x <144014593+himhim24x@users.noreply.github.com>
2024-03-16 02:46:28 +0800

    commonly used code to inc files (into inc folder)

---
+++ head.inc.php
@@ -0,0 +1,26 @@
+<head>
+    <meta charset="utf-8">
+    <title>Test Hotel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+    
+    <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+
+    <script src="js/bootstrap.min.js"></script>
+    
+    <script src="js/bootstrap-datepicker.js"></script> 
+    <script src="js/jquery.timepicker.min.js"></script> 
+
+    
+
+    <script src="js/main.js"></script>
+
+    <!-- CSS stylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+  </head>
---
+++ navbar.inc.php
@@ -0,0 +1,16 @@
+<div class="site-navbar js-site-navbar">
+              <nav role="navigation">
+                <div class="container">
+                  <div class="row full-height align-items-center">
+                    <div class="col-md-6 mx-auto">
+                      <ul class="list-unstyled menu">
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="rooms.php">Rooms</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="login.php">Login</a></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </nav>
+            </div>
