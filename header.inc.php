499ccdb01827228c05ed487170e2c256ee476dae
melwin911 <idkwhattoput30@gmail.com>
2024-03-30 23:00:43 +0800

    added google translate API
    added google translate API

--- header.inc.php
+++ header.inc.php
@@ -1,15 +1,14 @@
 <header class="site-header js-site-header">
-      <div class="container-fluid">
+    <div class="container-fluid">
         <div class="row align-items-center">
-          <div class="col-6 col-lg-4 site-logo" data-aos="fade"><a href="index.php">5 Hotel</a></div>
-          <div class="col-6 col-lg-8">
-
-          <div class="site-menu-toggle js-site-menu-toggle"  data-aos="fade">
-              <span></span>
-              <span></span>
-              <span></span>
-</div>
-          </div>
+            <div class="col-6 col-lg-4 site-logo" data-aos="fade"><a href="index.php">5 Hotel</a></div>
+            <div class="col-6 col-lg-8 d-flex justify-content-end align-items-center">
+                <div class="site-menu-toggle js-site-menu-toggle" data-aos="fade">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div id="google_translate_element"></div>
         </div>
-      </div>
+    </div>
 </header>
