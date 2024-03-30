a033175cf7c73db68871f51bef39d0e323ebc9fe
melwin911 <idkwhattoput30@gmail.com>
2024-03-30 20:30:24 +0800

    changed hotel name on title and head section
    changed hotel name on title and head section

--- head.inc.php
+++ head.inc.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8">
-    <title>Test Hotel</title>
+    <title>5 Hotel</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 
--- header.inc.php
+++ header.inc.php
@@ -1,7 +1,7 @@
 <header class="site-header js-site-header">
       <div class="container-fluid">
         <div class="row align-items-center">
-          <div class="col-6 col-lg-4 site-logo" data-aos="fade"><a href="index.php">Test Hotel</a></div>
+          <div class="col-6 col-lg-4 site-logo" data-aos="fade"><a href="index.php">5 Hotel</a></div>
           <div class="col-6 col-lg-8">
 
           <div class="site-menu-toggle js-site-menu-toggle"  data-aos="fade">
